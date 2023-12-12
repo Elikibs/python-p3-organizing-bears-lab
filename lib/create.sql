4b7e8068714618d3798d8319310c0fce735cf9eb
Elisha <elishakibet67@gmail.com>
2023-12-12 21:50:00 +0300

    Update project files

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  id INTEGET PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  sex TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 36, 'M', 'brown', 'moody', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 23, 'M', 'black', 'assertive', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 18, 'F', 'light', 'sensitive', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergant Brown', 45, 'M', 'black', 'courageous', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 28, 'F', 'light', 'persistent', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 40, 'M', 'brown', 'short-tempered', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 15, 'F', 'black', 'moody', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 26, 'F', 'light', 'assertive', 0);
